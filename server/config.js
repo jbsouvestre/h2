8c5034c2e7118fc122e28e95a00512928772e5ee
jb <jb@remotium.com>
2015-04-12 20:00:12 -0700

    config

---
+++ server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    /*jshint camelcase: false*/
+    consumer_key: 'h2alI96q5Pdya1JIJMUq78OUe',
+    consumer_secret: 'hhS9FXzGLMLpqNhEqHWH7HBLqFxe7C3NvOWAlLMdnuJGRlWkzu',
+    access_token_key: '323126288-FClQB843gkKTQEHQf7OATHdBXCLo3piKQexKV7HO',
+    access_token_secret: 'vnasMsy6FWbK4TKsOy699yqmWbhBhQHukKraGFrGr80Sw'
+};
