9e115cae613e42781c09fc741625be36e849dd25
jb <jb@remotium.com>
2015-04-13 08:11:30 -0700

    env variables

--- server/config.js
+++ server/config.js
@@ -1,7 +1,8 @@
+/* jshint node: true */
 module.exports = {
     /*jshint camelcase: false*/
-    consumer_key: 'h2alI96q5Pdya1JIJMUq78OUe',
-    consumer_secret: 'hhS9FXzGLMLpqNhEqHWH7HBLqFxe7C3NvOWAlLMdnuJGRlWkzu',
-    access_token_key: '323126288-FClQB843gkKTQEHQf7OATHdBXCLo3piKQexKV7HO',
-    access_token_secret: 'vnasMsy6FWbK4TKsOy699yqmWbhBhQHukKraGFrGr80Sw'
+    consumer_key: process.env['CONSUMER_KEY'],
+    consumer_secret: process.env['CONSUMER_SECRET'],
+    access_token_key: process.env['ACCESS_TOKEN_KEY'],
+    access_token_secret: process.env['ACCESS_TOKEN_SECRET']
 };
